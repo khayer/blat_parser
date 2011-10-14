b9e81862daae1578e2397c3d68da0a4e42574c70
Kaharina Hayer <katharinaehayer@gmail.com>
2011-10-14 16:53:51 -0400

    pslx needs to be added

--- lib/blat_parser/combine_fasta.rb
+++ lib/blat_parser/combine_fasta.rb
@@ -10,8 +10,25 @@ module BlatParser
     end
 
     def parse_to_file()
+      i = 0
+      str_n = ""
+      while i<50
+        str_n = "@{str_n}N"
+      end
+      f_out = File.new(@outdir,'w')
+
       while !f2.eof?()
-        if
+        line1 = @f1.readline().chomp
+        line2 = @f2.readline().chomp
+        if line1.include?('>')
+          str = "#{line1}\t#{line2}"
+        else
+          str = "#{line1}#{str_n}#{line2}"
+        end
+        f_out.write(str+"\n")
+      end
+
+
 
     end
 
