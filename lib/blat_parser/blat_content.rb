f63ecc538a0bd7ac36f4830447adc656e1bd35d6
Katharina Hayer <katharinaehayer@gmail.com>
2011-10-25 09:17:56 -0400

    only a few changes

--- lib/blat_parser/blat_content.rb
+++ lib/blat_parser/blat_content.rb
@@ -4,37 +4,56 @@ module BlatParser
     def initialize(line)
       tmp = line.split(" ")
       @matches = tmp[0]
-      @misMatches = tmp[1]
-      @repMatches = tmp[2]
-      @nCount = tmp[3]
-      @qNumInsert = tmp[4]
-      @qBaseInsert = tmp[5]
-      @tNumInsert = tmp[6]
-      @tBaseInsert = tmp[7]
+      @mis_matches = tmp[1]
+      @rep_matches = tmp[2]
+      @n_count = tmp[3]
+      @q_num_insert = tmp[4]
+      @q_base_insert = tmp[5]
+      @t_num_insert = tmp[6]
+      @t_base_insert = tmp[7]
       @strand = tmp[8]
-      @qname = tmp[9]
-      @qSize = tmp[10]
-      @qStart = tmp[11]
-      @qEnd = tmp[12]
-      @tName = tmp[13]
-      @tSize = tmp[14]
-      @tStart = tmp[15]
-      @tEnd = tmp[16]
-      @blockCount = tmp[17]
-      @blockSizes = tmp[18]
-      @qStarts = tmp[19]
-      @tStarts = tmp[20]
+      @q_name = tmp[9]
+      @q_size = tmp[10]
+      @q_start = tmp[11]
+      @q_end = tmp[12]
+      @t_name = tmp[13]
+      @t_size = tmp[14]
+      @t_start = tmp[15]
+      @t_end = tmp[16]
+      @block_count = tmp[17]
+      @block_sizes = tmp[18]
+      @q_starts = tmp[19]
+      @t_starts = tmp[20]
       @seq = tmp[21]
     end
 
-    attr_accessor :qname
+    attr_accessor :q_name, :t_name, :t_start, :t_end
 
     def to_s()
-      str = @matches+"\t"+@misMatches+"\t"+@repMatches+"\t"+@nCount+"\t"+@qNumInsert+"\t"+@qBaseInsert+"\t"+@tNumInsert+"\t"+@tBaseInsert+"\t"+@strand+"\t"+@qname+"\t"+@qSize+"\t"+@qStart+"\t"+@qEnd+"\t"+@tName+"\t"+@tSize+"/t"+@tStart+"/t"+@tEnd+"/t"+@blockCount+"/t"+@blockSizes+"/t"+@qStarts+"/t"+@tStarts+"/t"+@seq
+      str = @matches+"\t"+@mis_matches+"\t"+@rep_matches+"\t"+@n_count+"\t"+@q_num_insert+"\t"+@q_base_insert+"\t"+@t_num_insert+"\t"+@t_base_insert+"\t"+@strand+"\t"+@q_name+"\t"+@q_size+"\t"+@q_start+"\t"+@q_end+"\t"+@t_name+"\t"+@t_size+"/t"+@t_start+"/t"+@t_end+"/t"+@block_count+"/t"+@block_sizes+"/t"+@q_starts+"/t"+@t_starts+"/t"+@seq
     end
-
-
-
-
   end
 end
+
+# source: http://genome.ucsc.edu/FAQ/FAQformat.html#format2 , 10/24/2011 10:25 AM
+# matches - Number of bases that match that aren't repeats
+# misMatches - Number of bases that don't match
+# repMatches - Number of bases that match but are part of repeats
+# nCount - Number of 'N' bases
+# qNumInsert - Number of inserts in query
+# qBaseInsert - Number of bases inserted in query
+# tNumInsert - Number of inserts in target
+# tBaseInsert - Number of bases inserted in target
+# strand - '+' or '-' for query strand. For translated alignments, second '+'or '-' is for genomic strand
+# qName - Query sequence name
+# qSize - Query sequence size
+# qStart - Alignment start position in query
+# qEnd - Alignment end position in query
+# tName - Target sequence name
+# tSize - Target sequence size
+# tStart - Alignment start position in target
+# tEnd - Alignment end position in target
+# blockCount - Number of blocks in the alignment (a block contains no gaps)
+# blockSizes - Comma-separated list of sizes of each block
+# qStarts - Comma-separated list of starting positions of each block in query
+# tStarts - Comma-separated list of starting positions of each block in target#
--- lib/blat_parser/parser.rb
+++ lib/blat_parser/parser.rb
@@ -1,351 +1,154 @@
 module BlatParser
   class Parser
     include Enumerable
-    def initialize(filename = nil, outdir=nil, no_pointers=true)
-      if no_pointers
+
+    def initialize(filename , outdir, maxpairdist = 500000)
+
         @filehandler = ::File.open(filename)
         @outdir = outdir
 
+        #range in which a sequence is still considered unique
+        @maxpairdist = maxpairdist
+
+        @counter_unique = 0
+        @counter_non_unique = 0
+
         line = @filehandler.readline()
         a_line = aline = line.split()
+        # getting rid off heaerl
         while !is_a_number?(aline[0])
           line = @filehandler.readline()
           a_line = aline = line.split()
         end
-
-
-        parse_to_file_2(line)
-      else
-        @unique_mapper_pos = []
-        @non_unique_mapper_pos = []
-        @current_iteration = 0
-        @outdir = outdir
-        if filename
-          @filehandler = ::File.open(filename)
-          @list_of_lines = []
-          @list_of_header = []
-          position = @filehandler.pos
-          @filehandler.each do |line|
-            aline = line.split()
-            if !is_a_number?(aline[0])
-              @list_of_header << position
-            else
-              @list_of_lines << position
-            end
-            position = @filehandler.pos
-          end
-
-          self.parse_to_file()
-          @current_iteration = 0
-        else
-          @filehandler = nil
-          @list_of_lines = []
-          @list_of_header = []
-        end
+        parse_to_file(line)
       end
     end
 
     attr_accessor :list_of_lines, :current_iteration, :list_of_header, :unique_mapper_pos, :non_unique_mapper_pos
 
-    def next()
-      position = @list_of_lines[@current_iteration]
-      if position
-        @filehandler.pos = position
-      else
-        return nil
-      end
-      if @list_of_lines.length >= @current_iteration
-        @current_iteration += 1
-      else
-        return nil
-      end
-      make_content()
-    end
 
-    def make_content()
-      content = BlatParser::BlatContent.new(@filehandler.read())
-    end
 
-    def make_content_2(line)
+    def make_content(line)
       content = BlatParser::BlatContent.new(line)
     end
 
+    # Parses into unique and non-unique mappers
+    def parse_to_file(line)
+
+      z_unique = File.new(@outdir+"_unique", 'w')
+      z_non_unique = File.new(@outdir+"_non_unique", 'w')
+
+      while !@filehandler.eof?
+        entry1 = make_content(line)
+        line = @filehandler.readline()
+        entry2 = make_content(line)
+        if entry1.q_name == entry2.q_name
+          # What if on same chromosome?
+          # calling helper procedure
+            entries << entry1
+            marker2 = true
+          while entry1.q_name == entry2.q_name
+
+            if entry1.t_name == entry2.t_name
+
+              entries << entry2
+
+              if @filehandler.eof?
+                marker = false
+                break
+              else
+                line = @filehandler.readline()
+                entry2 = make_content(line)
+                marker = 1
+              end
+
+            else
+              marker2 = false
+
+              @counter_non_unique += 1
+
+              out = "#{entry1.to_s()}"
+              z_non_unique.write(out+"\n")
+
+              while entry1.qname == entry2.qname
+
+                out = "#{entry2.to_s()}"
+                z_non_unique.write(out+"\n")
+
+                if @filehandler.eof?
+                  marker = false
+                  break
+                else
+                  line = @filehandler.readline()
+                  entry2 = make_content(line)
+                  marker = 1
+                end
+              end
+
+            end
+
+            if marker2
+              is_in_range?(entries, z_unique, z_non_unique)
+            end
 
 
-    def each
-
-        for i in @list_of_lines
-          yield self.next()
-        end
-
-    end
-
-    # parse into unique and non-unique mappers
-
-    def parse()
-      @filehandler.pos = 0
-      #puts @filehandler.pos
-      qnames = []
-      pos = []
-
-      self.map  {|content|
-        #puts content.qname
-        qnames << content.qname()
-        pos << @current_iteration
-      }
-
-      counter_non_unique = 0
-      counter_unique = 0
-
-
-      while !qnames.empty?()
-
-        element1 = qnames.pop()
-        pos1 = pos.pop()
-
-        if qnames.include?(element1)
-          counter_non_unique += 1
-          @non_unique_mapper_pos << pos1
-          while qnames.include?(element1)
-            ind = qnames.index(element1)
-            qnames.delete_at(ind)
-            pos2 = pos.delete_at(ind)
-            @non_unique_mapper_pos << pos2
+          else
+            @counter_unique += 1
+            out = "#{entry1.to_s()}"
+            z_unique.write(out+"\n")
+            marker = false
           end
-        else
-          counter_unique += 1
-          @unique_mapper_pos << pos1
         end
-      end
-      puts "Unique: #{counter_unique}    Non_unique: #{counter_non_unique}"
+        if marker
+          @counter_unique += 1
+          out = "#{entry2.to_s()}"
+          z_unique.write(out+"\n")
+        end
+
+      puts "Unique: #{@counter_unique}    Non_unique: #{@counter_non_unique}"
+      z_non_unique.close
+      z_unique.close
     end
 
-    def content_at(x)
-      if x < 0
-        raise "Invalid entry number!"
-      end
-      if x > @list_of_lines.length()
-        raise "There are only #{@list_of_lines.length} entries!"
-      end
-      @current_iteration = x
-      self.next()
-    end
-
+    private
     # a little helper
     def is_a_number?(s)
       s.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true
     end
 
-    def non_unique_to_s()
-      entries_to_s(@non_unique_mapper_pos)
-    end
-
-    def unique_to_s()
-      entries_to_s(@unique_mapper_pos)
-    end
-
-
-    def parse_to_file()
-      @filehandler.pos = 0
-      #puts @filehandler.pos
-      qnames = []
-      pos = []
-
-
-      counter_non_unique = 0
-      counter_unique = 0
-      z_non_unique = File.new(@outdir+"_non_unique",'w')
-      z_unique = File.new(@outdir+"_unique", 'w')
-      out = ""
-
-      entry1 = self.next()
-      entry2 = self.next()
-
-      while entry2!=nil
-        #entry2 = self.next
-        if entry1.qname == entry2.qname
-          counter_non_unique += 1
-          out = "#{entry1.to_s()}"
-          z_non_unique.write(out+"\n")
-          while entry1.qname == entry2.qname
-            out = "#{entry2.to_s()}"
-            z_non_unique.write(out+"\n")
-            entry2 = self.next
-            if entry2 == nil
-              break
-            end
-          end
-        else
-          counter_unique += 1
-          out = "#{entry1.to_s()}"
-          z_unique.write(out+"\n")
-          entry1 = entry2
+    # only mappers in a certain range are seen as unique mappers
+    def is_in_range?(entries, z_unique, z_non_unique)
+      smallest = 2**(50)
+      biggest = 0
+      entries.each do |entry|
+        if entry.t_start > biggest
+          biggest = entry.t_start
+        end
+        if entry.t_end < smallest
+          smallest = entry.t_end
         end
       end
 
-      out = "#{entry1.to_s()}"
-      z_unique.write(out+"\n")
+      diff = biggest - smallest
 
-      puts "Unique: #{counter_unique}    Non_unique: #{counter_non_unique}"
-      z_non_unique.close
-      z_unique.close
-    end
-
-    def parse_to_file_2(line)
-
-      z_unique = File.new(@outdir+"_unique", 'w')
-      z_non_unique = File.new(@outdir+"_non_unique", 'w')
-
-      counter_non_unique = 0
-      counter_unique = 0
-
-
-
-      while !@filehandler.eof?
-         entry1 = make_content_2(line)
-         line = @filehandler.readline()
-         entry2 = make_content_2(line)
-
-         if entry1.qname == entry2.qname
-
-          counter_non_unique += 1
-          out = "#{entry1.to_s()}"
-          z_non_unique.write(out+"\n")
-
-          while entry1.qname == entry2.qname
-
-            out = "#{entry2.to_s()}"
-            z_non_unique.write(out+"\n")
-
-            if @filehandler.eof?
-              marker = false
-              break
-            else
-              line = @filehandler.readline()
-              entry2 = make_content_2(line)
-              marker = 1
-            end
-          end
-        else
-          counter_unique += 1
-          out = "#{entry1.to_s()}"
-          z_unique.write(out+"\n")
-          marker = false
-        end
+      if diff < @maxpairdist
+        # unique mapper
+        @counter_unique += 1
+        write_to_file(entries, z_unique)
+      else
+        # non-unique mapper
+        @counter_non_unique += 1
+        write_to_file(entries, z_non_unique)
       end
-      if marker
-        counter_unique += 1
-        out = "#{entry2.to_s()}"
-        z_unique.write(out+"\n")
-      end
-
-      puts "Unique: #{counter_unique}    Non_unique: #{counter_non_unique}"
-      z_non_unique.close
-      z_unique.close
-
-
 
     end
 
-
-
-
-
-    def parse_to_file_3(filename, outdir)
-
-      @filehandler = File.new(filename, 'r')
-      z_unique = File.new(outdir+"_unique", 'w')
-      z_non_unique = File.new(outdir+"_non_unique", 'w')
-
-      counter_non_unique = 0
-      counter_unique = 0
-
-
-      while !@filehandler.eof?
-        line = @filehandler.readline()
-        aline = line.split()
-        if is_a_number?(aline[0])
-          entry1 = make_content_2(line)
-          #puts entry1.qname
-          if @filehandler.eof?()
-            counter_unique += 1
-            out = "#{entry1.to_s()}"
-            z_unique.write(out+"\n")
-            break
-          else
-
-
-            while entry1 != nil
-              line = @filehandler.readline()
-              entry2 = make_content_2(line)
-
-
-              if entry1.qname == entry2.qname
-                counter_non_unique += 1
-                out = "#{entry1.to_s()}"
-                z_non_unique.write(out+"\n")
-                while entry1.qname == entry2.qname
-
-                  out = "#{entry2.to_s()}"
-                  z_non_unique.write(out+"\n")
-                  if !@filehandler.eof?()
-                    line = @filehandler.readline()
-                    entry2 = make_content_2(line)
-                  else
-                    entry2 = nil
-                  end
-                end
-                entry1 = entry2
-                if @filehandler.eof?()
-                  counter_unique += 1
-                  out = "#{entry1.to_s()}"
-                  z_unique.write(out+"\n")
-                end
-                  #line = @filehandler.readline()
-                  #entry2 = make_content_2(line)
-                #else
-                  #counter_unique += 1
-                #else
-                  #line = @filehandler.readline()
-                  #entry2 = make_content_2(line)
-                #end
-              else
-                counter_unique += 1
-                out = "#{entry1.to_s()}"
-                z_unique.write(out+"\n")
-                entry1 = entry2
-              end
-            end
-          end
-        end
+    def write_to_file(entries, filehandler)
+      entries.each do |entry|
+        out = "#{entry.to_s()}"
+        filehandler.write(out+"\n")
       end
-
-
-      puts "Unique: #{counter_unique}    Non_unique: #{counter_non_unique}"
-      z_non_unique.close
-      z_unique.close
     end
-
-
-
-    private
-    def entries_to_s(positions)
-      z = File.new(@outdir,'w')
-      out = ""
-
-      for pos in positions
-          pos = pos - 1
-          entry = content_at(pos)
-          out = "#{entry.to_s()}"
-          z.write(out+"\n")
-      end
-
-      z.close
-    end
-
-
-
-
 
   end
 end
